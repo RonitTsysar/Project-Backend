6f8d216a0d08bc26bc948f09dde418ca190ae59e
guy <guyklinger94@gmail.com>
2021-05-28 15:49:07 +0300

    changes

--- project/utils/far_utils.js
+++ project/utils/far_utils.js
@@ -1,6 +1,10 @@
 const DButils = require("../utils/DButils");
 
 async function checkIfFarIsValid(userId){
+    if(userId === undefined){
+        return false;
+    }
+
     const referee = await DButils.execQuery(
         // `select ifnull((select refereeId from referees where refereeId=${userId}), No Result Found)`
         `select * from FARs where FARId=${userId}`
@@ -8,6 +12,7 @@ async function checkIfFarIsValid(userId){
     if(referee[0] === undefined){
         return false;
     }
+    
     return true;
 }
 
