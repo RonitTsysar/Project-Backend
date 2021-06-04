4957373810c902645c9f14eacf0be5f7df7dd0bb
DanaKlim <dana1997k@gmail.com>
2021-06-04 13:00:00 +0300

    adding jest to devDependencies

--- project/tests/auth.test.js
+++ project/tests/auth.test.js
@@ -1,5 +1,6 @@
 const auth_domain = require("../domain/auth_domain");
 
+
 // test('check a proper login of a user, positive test', () => {
 //     expect(auth_domain.loginUser("ronit", "lady@16").not.toEqual(null))
 // })
