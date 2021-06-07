febd09b3474d608c84413e62be4721683075151c
DanaKlim <dana1997k@gmail.com>
2021-06-07 16:40:18 +0300

    Finished all Login checks

--- project/tests/auth_domain.test.js
+++ project/tests/auth_domain.test.js
@@ -1,22 +1,22 @@
 const auth_domain = require("../domain/auth_domain");
 
-// *****************************************ACCEPTANCE TESTING - LOGIN USE CASE *******************************************************
+// *****************************************LOGIN USE CASE *******************************************************
 
-test('Check a successful login of a registered user', async () => {
+test('Check a valid login of a user', async () => {
     let userName = "danaKlim";
     let password = "dana123";
     const user = await auth_domain.loginUser(userName, password);
     expect(user).not.toBeNull();
 });
 
-test('Check an unsuccessful login of a registered user', async () => {
+test('Check an unsuccessful login - wrong password', async () => {
     let userName = "danaKlim";
     let password = "dana1234";
     const user = await auth_domain.loginUser(userName, password);
     expect(user).toBeNull();
 });
 
-test('Check an unsuccessful login of a registered user', async () => {
+test('Check an unsuccessful login - wrong user name', async () => {
     let userName = "danaKlimenko";
     let password = "dana123";
     const user = await auth_domain.loginUser(userName, password);
